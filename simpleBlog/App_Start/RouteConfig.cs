339b58bbd8252716f912a1f9271ccbe68f8f8fe1
Hasitha <hasithamadusanka90@gmil.com>
2016-06-20 21:07:01 +0530

    inital commet for bitB

--- simpleBlog/App_Start/RouteConfig.cs
+++ simpleBlog/App_Start/RouteConfig.cs
@@ -9,7 +9,6 @@ namespace simpleBlog
 {
     public class RouteConfig
     {
-
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");

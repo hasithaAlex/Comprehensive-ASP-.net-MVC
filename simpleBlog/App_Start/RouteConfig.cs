db69d447d5eb3b6245f700f316f5db12ae93277b
Hasitha <hasithamadusanka90@gmil.com>
2016-06-27 16:25:47 +0530

    7. Routes, Areas, and Controllers

--- simpleBlog/App_Start/RouteConfig.cs
+++ simpleBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using simpleBlog.Controllers;
 
 namespace simpleBlog
 {
@@ -11,13 +12,14 @@ namespace simpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+
+            var namespaces = new[] {typeof (PostsController).Namespace};
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "login" }, namespaces);
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "index" }, namespaces);
         }
     }
 }

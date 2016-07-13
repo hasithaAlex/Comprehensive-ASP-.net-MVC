ec99102f4b80460d62015b6ed4d663c8ec7d3ad4
Hasitha <hasithamadusanka90@gmil.com>
2016-07-13 11:20:46 +0530

    11. Scripts, Styles and Bundles

---
+++ simpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Optimization;
+using System.Web.UI.WebControls;
+
+namespace simpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css")
+                );
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-1.10.2.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-1.10.2.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+            
+        }
+    }
+}

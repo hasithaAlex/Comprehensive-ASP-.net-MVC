4197113dfa1995132c1eccefe71c2f6756d2d089
Hasitha <hasithamadusanka90@gmil.com>
2016-08-27 14:20:46 +0530

    20. Preventing CSRF (Cross-site Request Forgery) Attacks

--- simpleBlog/Areas/Admin/Controllers/UserController.cs
+++ simpleBlog/Areas/Admin/Controllers/UserController.cs
@@ -33,7 +33,7 @@ namespace simpleBlog.Areas.Admin.Controllers
             });
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult New(UsersNew form)
         {
             if(Database.Session.Query<User>().Any(u => u.UserName == form.Username))
@@ -69,7 +69,7 @@ namespace simpleBlog.Areas.Admin.Controllers
             });
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Edit(int id,UsersEdit form)
         {
             var user = Database.Session.Load<User>(id);
@@ -103,7 +103,7 @@ namespace simpleBlog.Areas.Admin.Controllers
         }
 
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult ResetPassword(int id, UsersResetPassword form)
         {
             var user = Database.Session.Load<User>(id);
@@ -121,7 +121,7 @@ namespace simpleBlog.Areas.Admin.Controllers
             return RedirectToAction("index");
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
         {
             var user = Database.Session.Load<User>(id);
